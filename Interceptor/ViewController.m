6ffcfd5393e8bdb10eaf73fdec79bc9d42286888
Jowyer <Jowyer@gmail.com>
2016-05-23 16:48:58 +0800

    first commit

---
+++ Interceptor/ViewController.m
@@ -0,0 +1,18 @@
+//
+//  ViewController.m
+//  Interceptor
+//
+//  Created by casa on 4/17/15.
+//  Copyright (c) 2015 alibaba. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+/*
+ 
+    业务方也不需要写任何迎合框架的代码
+ */
+
+@end
